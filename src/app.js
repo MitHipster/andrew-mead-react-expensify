0bb11e1fbb7879651dd886f7f43decc551177434
Tim Acker <tjacker31@gmail.com>
2018-06-17 16:31:25 -0400

    React Router
    Basic reacter router setup to test configuration.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => <div>This is my dashboard component.</div>;
+const AddExpensePage = () => <div>This is my add expense component.</div>;
+const EditExpensePage = () => <div>This is my edit expense component.</div>;
+const HelpPage = () => <div>This is my help component.</div>;
+
+const routes = (
+	<Router>
+		<div>
+			<Route exact path="/" component={ExpenseDashboardPage} />
+			<Route path="/create" component={AddExpensePage} />
+			<Route path="/edit" component={EditExpensePage} />
+			<Route path="/help" component={HelpPage} />
+		</div>
+	</Router>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
