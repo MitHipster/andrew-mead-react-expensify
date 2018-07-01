7a63a817abfa3238cb4d8b3d371e8bc2d5dc58bd
Tim Acker <tjacker31@gmail.com>
2018-06-30 22:04:34 -0400

    Date Range Picker
    Added a date range picker to filter out expenses from the dashboard page.

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,15 +1,26 @@
+import moment from 'moment';
+
 export default (expenses, { text, sortBy, startDate, endDate }) => {
-	return expenses.filter((expense) => {
-		const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-		const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate
-		const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
-		return startDateMatch && endDateMatch && textMatch;
-	}).sort((a, b) => {
-		if (sortBy === 'date') {
-			return b.createdAt - a.createdAt;
-		}
-		if (sortBy === 'amount') {
-			return b.amount - a.amount;
-		}
-	});
+	return expenses
+		.filter(expense => {
+			const createdAtMoment = moment(expense.createdAt);
+			const startDateMatch = startDate
+				? startDate.isSameOrBefore(createdAtMoment, 'day')
+				: true;
+			const endDateMatch = endDate
+				? endDate.isSameOrAfter(createdAtMoment, 'day')
+				: true;
+			const textMatch = expense.description
+				.toLowerCase()
+				.includes(text.toLowerCase());
+			return startDateMatch && endDateMatch && textMatch;
+		})
+		.sort((a, b) => {
+			if (sortBy === 'date') {
+				return b.createdAt - a.createdAt;
+			}
+			if (sortBy === 'amount') {
+				return b.amount - a.amount;
+			}
+		});
 };
