00aff629ad48cf2f6936d54d847cdfc96470ee83
Tim Acker <tjacker31@gmail.com>
2018-08-04 21:32:27 -0400

    Add Expense Refactor
    Refactored store to allow functions to be returned and refactored AddExpensePage to push a new expense to Firebase.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,22 +1,8 @@
 import * as firebase from 'firebase';
 import config from './config';
-console.log(config);
 
 firebase.initializeApp(config);
 
 const db = firebase.database();
 
-// Fires when a record is added
-db.ref('expenses').on('child_added', snapshot => {
-	console.log(snapshot.key, snapshot.val());
-});
-
-// Fires when a record is changed
-db.ref('expenses').on('child_changed', snapshot => {
-	console.log(snapshot.key, snapshot.val());
-});
-
-// Fires when a record is removed
-db.ref('expenses').on('child_removed', snapshot => {
-	console.log(snapshot.key, snapshot.val());
-});
+export { firebase, db as default };
