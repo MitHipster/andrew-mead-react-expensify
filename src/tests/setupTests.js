93364f4e25f1279e294cfe15a2c9f3be8c73cfc1
Tim Acker <tjacker31@gmail.com>
2018-08-05 22:09:27 -0400

    Setup Environments
    Setup environment .env files for testing and developement and created a separate Firebase test database.

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,6 +1,9 @@
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import dotenv from 'dotenv';
 
 enzyme.configure({
 	adapter: new Adapter()
 });
+
+dotenv.config({ path: '.env.test' });
